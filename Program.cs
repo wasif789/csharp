fff3c778c6cf4746a13ae86ffaed86bb80e14384
wasif farid <wsffarid1@gmail.com>
2021-08-28 08:33:19 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double sum = (a + b);
+            Console.WriteLine("sum is :" + sum);
+
+        }
+    }
+}
