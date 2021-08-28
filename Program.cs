126ce8922b85bc03f6f43452bc541722ca58323e
wasif789 <88643198+wasif789@users.noreply.github.com>
2021-08-28 08:45:21 +0530

    Update Program.cs
    hello message added

--- Program.cs
+++ Program.cs
@@ -6,12 +6,7 @@ namespace Addition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter first number");
-            double a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("enter second number");
-            double b = Convert.ToDouble(Console.ReadLine());
-            double sum = (a + b);
-            Console.WriteLine("sum is :" + sum);
+            Console.WriteLine("hello world");
 
         }
     }
